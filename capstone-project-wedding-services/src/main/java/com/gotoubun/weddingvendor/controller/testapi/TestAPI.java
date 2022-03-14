baebdd715022423976b0c3d11e922b5dcead6ad6
ShinCN <shincnielts@gmail.com>
2022-03-14 20:40:14 +0700

    update test api 14/3

--- capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/controller/testapi/TestAPI.java
+++ capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/controller/testapi/TestAPI.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestAPI {
 
-    @GetMapping("/")
+    @GetMapping("/ok")
     public String testAPI(){
         return "Ok luon";
     }

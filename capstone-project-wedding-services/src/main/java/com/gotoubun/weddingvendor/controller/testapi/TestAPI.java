55554a401c71f6734cf4c88963982e63751f8966
ShinCN <shincnielts@gmail.com>
2022-03-14 21:05:41 +0700

    message API

--- capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/controller/testapi/TestAPI.java
+++ capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/controller/testapi/TestAPI.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestAPI {
 
-    @GetMapping("/ok")
+    @GetMapping("/message")
     public String testAPI(){
         return "Ok luon";
     }

5e93f54bcc75f73cff0877e77eeabfe44886dea0
ShinCN <shincnielts@gmail.com>
2022-03-19 13:59:40 +0700

    run đi thằng deploy kia

--- capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/repository/VendorRepository.java
+++ capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/repository/VendorRepository.java
@@ -11,7 +11,7 @@ import com.gotoubun.weddingvendor.entity.user.VendorProvider;
 
 @Repository
 public interface VendorRepository extends JpaRepository<VendorProvider, Long> {
-	@Query(value = "Select b FROM VendorProvider b WHERE b.address LIKE %:searchText% OR b.mail LIKE %:searchText% OR b.phone LIKE %:searchText% OR b.username LIKE %:searchText%")
+	@Query(value = "Select b FROM VendorProvider b WHERE b.address LIKE %:searchText% OR b.mail LIKE %:searchText% OR b.phone LIKE %:searchText%")
 	Page<VendorProvider> findAllVendors(Pageable pageable, @Param("searchText") String searchText);
 
 }

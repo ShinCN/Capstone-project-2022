43ab9158f9dab8ecf70a5bb5d049f0a0e5c774ad
= <=>
2022-04-04 23:24:30 +0700

    fix abcxyz

--- capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/security/SecurityConfig.java
+++ capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/security/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.gotoubun.weddingvendor.security;
 
 import com.gotoubun.weddingvendor.filter.CorsFilter;
 import com.gotoubun.weddingvendor.service.common.CustomAccountDetailsService;
-import com.sun.tools.javac.util.List;
-import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -57,23 +55,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManager();
     }
 
-        @Bean
-        CorsFilter corsFilter() {
-            CorsFilter filter = new CorsFilter();
-            return filter;
-        }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .configurationSource(request -> {
-                    var cors = new CorsConfiguration();
-                    cors.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:80", "http://example.com"));
-                    cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    cors.setAllowedHeaders(List.of("*"));
-                    return cors;
-                }).
+                .cors().
                 and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement()

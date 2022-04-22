eabb528a3425fa27da1ab8caa3256f113fce7938
= <=>
2022-04-23 01:31:07 +0700

    update api about sort by date

--- capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/resource/planningtools/CheckListTaskController.java
+++ capstone-project-wedding-services/src/main/java/com/gotoubun/weddingvendor/resource/planningtools/CheckListTaskController.java
@@ -92,7 +92,7 @@ public class CheckListTaskController {
             Principal principal,
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
+            @RequestParam(value = "sortBy", defaultValue = "dueDate", required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir) {
         // TODO Auto-generated method stub
         //check login
